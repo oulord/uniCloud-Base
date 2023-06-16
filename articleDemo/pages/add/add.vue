78c3f72bf3f6e0ce8aacaef9e68657d33f0ed3c6
guo <2246338405@qq.com>
2023-06-16 20:19:01 +0800

    小案例第五次提交
    完成判断内容为空不准发布及发布成功后的跳转

--- articleDemo/pages/add/add.vue
+++ articleDemo/pages/add/add.vue
@@ -2,20 +2,20 @@
 	<view class="add">
 		<form @submit="onSubmit">
 			<view class="item">
-				<input type="text" name="title" placeholder="请输入标题" />
+				<input v-model="formValue.title" type="text" name="title" placeholder="请输入标题" />
 			</view>
 
 			<view class="item">
-				<input type="text" name="author" placeholder="请输入作者" />
+				<input v-model="formValue.author" type="text" name="author" placeholder="请输入作者" />
 			</view>
 
 			<view class="item">
-				<textarea name="content" placeholder="请输入详细内容"></textarea>
+				<textarea v-model="formValue.content" name="content" placeholder="请输入详细内容"></textarea>
 			</view>
 
 			<view class="item">
 				<button form-type="reset">重置</button>
-				<button form-type="submit" type="primary">提交</button>
+				<button form-type="submit" type="primary" :disabled="inDisabled(formValue)">提交</button>
 			</view>
 		</form>
 	</view>
@@ -25,11 +25,24 @@
 	export default {
 		data() {
 			return {
-
+				formValue: {
+					title: "",
+					author: "",
+					content: "",
+				}
 			};
 		},
 
 		methods: {
+			// 判断按钮是否禁用
+			inDisabled(obj) {
+				for (let key in obj) {
+					if (!obj[key]) {
+						return true
+					}
+				}
+			},
+
 			// 点击提交表单
 			onSubmit(e) {
 				let detail = e.detail.value
@@ -39,7 +52,15 @@
 						detail: detail
 					}
 				}).then(res => {
-					console.log(res);
+					// console.log(res);
+					uni.showToast({
+						title: "发布成功",
+					})
+					setTimeout(() => {
+						uni.reLaunch({
+							url: "/pages/index/index"
+						})
+					}, 15000)
 				})
 			}
 		}
